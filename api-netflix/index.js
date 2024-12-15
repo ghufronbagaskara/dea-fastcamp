df2afb724437f038be28870b6dfc17decd013b0c
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2024-12-15 13:07:12 +0700

    feat: learn makin some endpoint

--- api-netflix/index.js
+++ api-netflix/index.js
@@ -4,12 +4,40 @@ const app = express()
 const PORT = 3002
 
 app.get("/", (request, response) => {
-    response.json({ message: "Hello World API" })
+    response.status(200).json({ message: "Hello World API" })
 })
 
-app.get("/movies", (request, response) => {
+// Melihat list favorite movie
+app.get("/my-movies", (request, response) => {
     console.log("Movies API get hit!");
     response.json({ data: "Dilan 2020" })
+
+})
+
+// Menambah favorite movie
+app.post("/my-movies", (request, response) => {
+    console.log("insert new favorite movie..");
+    const data = request.body
+    console.log({ data });
+    response.status(201).json({ message: "success" })
+
+})
+
+// Menghapus favorite movie
+app.delete("/my-movies/:id/:token", (request, response) => {
+    console.log("delete favorite movie..");
+    const { id, token } = request.params
+    console.log({ id, token });
+    response.status(204).json({ message: "success" })
+
+})
+
+// Menambah data user untuk verifikasi sistem auth
+app.post("/token", (request, response) => {
+    console.log("creating user token..");
+    const data = response.body
+    console.log({ data });
+    response.status(201).json({ message: "token created.." })
 
 })
 
