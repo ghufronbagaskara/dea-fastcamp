b73f28a2f85a23fda90a973232fcd4822bdde4f4
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2024-12-15 12:48:06 +0700

    add: nodemon, cors at nodemon

--- api-netflix/index.js
+++ api-netflix/index.js
@@ -7,6 +7,12 @@ app.get("/", (request, response) => {
     response.json({ message: "Hello World API" })
 })
 
+app.get("/movies", (request, response) => {
+    console.log("Movies API get hit!");
+    response.json({ data: "Dilan 2020" })
+
+})
+
 app.listen(PORT, () => {
     console.log("Server API berjalan di PORT " + PORT);
 })
