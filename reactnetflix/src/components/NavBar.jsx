42e8df809d89ccacce6258d58980aeb800b58dee
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2024-12-16 17:58:14 +0700

    feat: adding framer motion conf + impl

--- reactnetflix/src/components/NavBar.jsx
+++ reactnetflix/src/components/NavBar.jsx
@@ -9,7 +9,7 @@ function NavBar() {
             <h3>Netflix</h3>
             <div>
                 <button className="btn btn-accent btn-active mr-4" onClick={() => setLanguage("id")}>Indonesia</button>
-                <button onClick={() => setLanguage("en")}>English</button>
+                <button className="active:bg-red-500 hover:bg-yellow-500" onClick={() => setLanguage("en")}>English</button>
             </div>
         </div>
     )
--- reactnetflix/src/pages/Home.jsx
+++ reactnetflix/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useAtom } from 'jotai';
 import { languageAtom } from '../jotai/atoms';
 import { useEffect, useState } from 'react';
 import { apiInstance } from '../utils/apiInstance';
+import { anticipate, motion } from 'framer-motion';
 
 function Home() {
   const [language,] = useAtom(languageAtom)
@@ -21,8 +22,35 @@ function Home() {
 
   return (
     <Layout>
-      <h1>{language == "en" ? "Watch Netflix Free!" : "Tonton Netflix Gratis!"}</h1>
-      <p className='bg-black text-white p-4 text-center'>Message from api : {movies?.message}</p>
+      <motion.h1
+        initial={{ x: "100%" }}
+        animate={{ x: "0%" }}
+        transition={{ duration: 2, ease: "easeIn" }}
+      >{language == "en" ? "Watch Netflix Free!" : "Tonton Netflix Gratis!"}</motion.h1>
+      <div className='w-3/4 '>
+        <p className='text-center text-indigo-500 font-normal text-sm m-10 p-8 bg-[#e1e1e1] hover:bg-blue-300 hover:text-red-500 '>Message from api : {movies?.message}</p>
+      </div>
+      <motion.div
+        initial={{ y: "100%", opacity: 0 }}
+        animate={{ y: "0%", opacity: 1 }}
+        transition={{ duration: 1, ease: 'anticipate' }}
+        className='grid grid-cols-2 gap-4'>
+        <div className='bg-indigo-500 p-4'>
+          <h1 >Hello World</h1>
+        </div>
+
+        <div className='bg-indigo-500 p-4'>
+          <h1>Hello World</h1>
+        </div>
+
+        <div className=' bg-indigo-500 p-4'>
+          <h1>Hello World</h1>
+        </div>
+
+        <div className=' bg-indigo-500 p-4'>
+          <h1>Hello World</h1>
+        </div>
+      </motion.div>
     </Layout>
   );
 }
